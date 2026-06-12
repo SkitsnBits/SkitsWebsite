f67262c914d895e66bdb5d70a8ed088483ee31ad
Modulorium <modulorius@gmail.com>
2026-06-12 16:15:07 -0400

    route root to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { HomePageComponent } from './page/home-page/home-page.component';
 import { StaffPageComponent } from './page/staff-page/staff-page.component';
 
 export const routes: Routes = [
-    { path: 'test', component: HomePageComponent },
-    { path: '', component: WipComponent },
+    { path: '', component: HomePageComponent },
+    { path: 'wip', component: WipComponent },
     { path: 'staff', component: StaffPageComponent },
     { path: '**', redirectTo: '/' },
 ];
